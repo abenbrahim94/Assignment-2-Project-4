d5438a503cd1770173be3e28b09a50b6bbfa7e12
abenbrahim94 <abenbrahim94@gmail.com>
2015-02-13 21:10:31 -0500

    Assignment2Project4Final

---
+++ Assignment2Project4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2Project4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            double HOL; // Height of the Lawn
+            double WOL; // Width of the Lawn
+            double SFOL; // Square feet of Lawn
+            double SeosonalFee1;
+            double SeosonalFee2;
+            double SeosonalFee3;
+            //Codes
+            Console.WriteLine("==========================Assignment2 project4=========================");
+            Console.WriteLine("Mowing Services ");
+            Console.WriteLine("=======================================================================");
+            Console.Write("\n");
+            Console.WriteLine("Insert the Height of your Lawn (feet):  ");
+            HOL = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insert the Width of your Lawn (feet): ");
+            WOL = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            SFOL = HOL * WOL;
+            SeosonalFee1 = 25 * 20;
+            SeosonalFee2 = 35 * 20;
+            SeosonalFee3 = 50 * 20;
+            if (SFOL < 400)
+            {
+                Console.WriteLine("The Square Feet of Your Lawn is {0} Sq.Ft.", SFOL);
+                Console.WriteLine("The Weekly fee of Mowing Your Lawn is $25");
+                Console.WriteLine("The fee of a 20 week season for your Lawn is {0:c}", SeosonalFee1);
+
+            }
+            else if ((SFOL >= 400) && (SFOL < 600))
+            {
+                Console.WriteLine("The Square Feet of Your Lawn is {0} Sq.Ft.", SFOL);
+                Console.WriteLine("The Weekly fee of Mowing Your Lawn is $35");
+                Console.WriteLine("The fee of a 20 week season for your Lawn is {0:c}", SeosonalFee2);    
+            } 
+            else
+            {
+                Console.WriteLine("The Square Feet of Your Lawn is {0} Sq.Ft.", SFOL);
+                Console.WriteLine("The Weekly fee of Mowing Your Lawn is $50");
+                Console.WriteLine("The fee of a 20 week season for your Lawn is {0:c}", SeosonalFee3);
+            }
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit ...");
+            Console.ReadKey();
+        }
+    }
+}
